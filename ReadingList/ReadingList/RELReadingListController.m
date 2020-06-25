9fbdbf14580581e35ca7a83b2e775c3e53cec166
Jonathan Lehr <jonathan@aboutobjects.com>
2020-06-25 04:52:42 -0400

    Added ReadingList app

---
+++ ReadingList/ReadingList/RELReadingListController.m
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "RELReadingListController.h"
+
+@implementation RELReadingListController
+
+- (IBAction)done:(UIStoryboardSegue *)unwindSegue {
+    // TODO: Sync UI and save
+    NSLog(@"In %s", __func__);
+}
+
+- (IBAction)cancel:(UIStoryboardSegue *)unwindSegue {
+    
+}
+
+@end
